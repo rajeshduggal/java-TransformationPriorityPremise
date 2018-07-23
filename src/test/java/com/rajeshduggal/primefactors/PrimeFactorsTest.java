09ffe2670bbe392493f075470fa042a796289510
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-23 19:54:16 -0400

    PrimeFactorsTest: Simplify code with static imports

--- src/test/java/com/rajeshduggal/primefactors/PrimeFactorsTest.java
+++ src/test/java/com/rajeshduggal/primefactors/PrimeFactorsTest.java
@@ -1,12 +1,13 @@
 package com.rajeshduggal.primefactors;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Arrays;
+import static com.rajeshduggal.primefactors.PrimeFactors.of;
+import static java.util.Arrays.asList;
 import org.junit.Test;
 
 public class PrimeFactorsTest {
     @Test
     public void primeFactors() throws Exception {
-        assertEquals(Arrays.asList(), PrimeFactors.of(1));
+        assertEquals(asList(), of(1));
     }
 }
